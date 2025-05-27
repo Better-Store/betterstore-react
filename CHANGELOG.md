00d16a76dce0823111e646a12cfe3764d6857e64
pavelbuchta <pavelbuchta80@gmail.com>
2025-05-27 21:19:23 +0200

    bug fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @betterstore/sdk
 
+## 0.3.31
+
+### Patch Changes
+
+- bug fixes
+
 ## 0.3.30
 
 ### Patch Changes
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  corePlugins: {
-    preflight: false, // disable Tailwind's base styles
-  },
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   important: true, // This ensures your styles take precedence
   theme: {
